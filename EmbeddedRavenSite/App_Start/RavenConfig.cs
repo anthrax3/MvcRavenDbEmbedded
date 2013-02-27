6ffddd3f4df20fd3058875976245375ca3a48136
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2013-02-27 08:59:46 -0500

    initial commit.

---
+++ EmbeddedRavenSite/App_Start/RavenConfig.cs
@@ -0,0 +1,18 @@
+﻿using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace EmbeddedRavenSite.App_Start
+{
+    public static class RavenConfig
+    {
+        public static IDocumentStore Init()
+        {
+            return new EmbeddableDocumentStore
+            {
+                ConnectionStringName = "RavenDb",
+                UseEmbeddedHttpServer = true
+            }.Initialize();
+
+        }
+    }
+}
